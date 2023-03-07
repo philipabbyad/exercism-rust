0e3bbf3af38b55c24cbcc3ccf79cf83194dbdda1
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-03-06 17:39:24 -0700

    Added error handling to production_rate_per_hour

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,14 +1,15 @@
 const BASE_PRODUCTION_SPEED_PER_HOUR: f64 = 221.0;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    BASE_PRODUCTION_SPEED_PER_HOUR * speed as f64 * match speed {
+    BASE_PRODUCTION_SPEED_PER_HOUR * (speed as f64) * match speed {
+        0 => 0.0,
         1..=4 => 1.0,
         5..=8 => 0.9,
         9 | 10 => 0.77,
-        _ => 0.0,
+        _ => panic!("Invalid speed: {}", speed)
     }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    production_rate_per_hour(speed) as u32 / 60
+    (production_rate_per_hour(speed) as u32) / 60
 }
