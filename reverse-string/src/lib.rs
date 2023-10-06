5b7fa6c42f0ad8c4976523b55886b60cb5de1977
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-10-06 15:25:48 -0600

    Continued reverse string exercise

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {input}");
+ //   let reversed_string = input.to_string();
+ //   reversed_string.to
+ //   return reversed_string
+    let characters = input.chars();
+ //   input.push_str('a');
+   // let reversed_string = characters.toString();
+    let last_character: String = characters.next_back();
+    let reversed_string = last_character.toString();
 }
